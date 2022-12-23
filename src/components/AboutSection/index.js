671552c2b08f747df228d027a1a214a4cac9a4c8
userwijnja <56116163+userwijnja@users.noreply.github.com>
2022-12-23 14:20:14 +0100

    Update index.js

--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -1,48 +1,74 @@
-import React from 'react'
-import {Button} from '../ButtonElements';
+import React from "react";
+import { Button } from "../ButtonElements";
 
-import { AboutContainer, AboutWrapper, AboutRow, Column1, Column2, TextWrapper, TopLine, Heading, Subtitle, BtnWrap, ImgWrap, Img } from './AboutElements'
+import {
+  AboutContainer,
+  AboutWrapper,
+  AboutRow,
+  Column1,
+  Column2,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Subtitle,
+  BtnWrap,
+  ImgWrap,
+  Img,
+} from "./AboutElements";
 
-const AboutSection = ({lightBg, id, imgStart, topLine, lightText, headLine, darkText, description, description2, description3, buttonLabel, img, primary, alt, dark, dark2}) => {
-    return (
-        <>
+const AboutSection = ({
+  lightBg,
+  id,
+  imgStart,
+  topLine,
+  lightText,
+  headLine,
+  darkText,
+  description,
+  description2,
+  description3,
+  buttonLabel,
+  img,
+  primary,
+  alt,
+  dark,
+  dark2,
+}) => {
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
 
-            <AboutContainer lightBg={lightBg} id={id}>
-                <AboutWrapper>
-                    <AboutRow imgStart={imgStart}>
-                        <Column1>
-                            <TextWrapper>
-                                <TopLine>{topLine}</TopLine>
-                                <Heading lightText={lightText}>{headLine}</Heading>
-                                <Subtitle darkText={darkText}>{description}</Subtitle>
-                                <Subtitle darkText={darkText}>{description2}</Subtitle>
-                                <Subtitle darkText={darkText}>{description3}</Subtitle>
-                              <BtnWrap>
-        <Button to='werkwijze'
-        smooth={true}
-        duration={500}
-        spy={true}
-        exact="true"
-        offset={-80}
-        primary={primary ? 1 : 0}
-        dark={dark ? 1 : 0}
-        dark2={dark2 ? 1: 0}
-        >
-            {buttonLabel}
-        </Button>
-        </BtnWrap>
-                   </TextWrapper>
-                        </Column1>
-                        <Column2>
-                            <ImgWrap>
-                                <Img src={img} alt={alt}/>
-                            </ImgWrap>
-                        </Column2>
-                    </AboutRow>
-                </AboutWrapper>
-            </AboutContainer>
-        </>
-    )
-}
+  if (hours < 12) {
+    timeOfDay = "morgen";
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "middag";
+  } else {
+    timeOfDay = "navond";
+  }
+  return (
+    <>
+      <AboutContainer lightBg={lightBg} id={id}>
+        <TopLine>{topLine}</TopLine>
+        <AboutWrapper>
+          <AboutRow imgStart={imgStart}>
+            <Column1>
+              <TextWrapper>
+                <Subtitle>Goede{timeOfDay}! </Subtitle>
+                <Subtitle darkText={darkText}>{description}</Subtitle>
+                <Subtitle darkText={darkText}>{description2}</Subtitle>
+                <Subtitle darkText={darkText}>{description3}</Subtitle>
+              </TextWrapper>
+            </Column1>
+            <Column2>
+              <ImgWrap>
+                <Img src={img} alt={alt} />
+              </ImgWrap>
+            </Column2>
+          </AboutRow>
+        </AboutWrapper>
+      </AboutContainer>
+    </>
+  );
+};
 
-export default AboutSection
+export default AboutSection;
